c43ba052fdafc1ba4ccccc28299302a5c0cb3d1f
Erbel, Marcin <Marcin.Erbel@harman.com>
2019-07-24 10:36:13 +0200

    init

---
+++ spring-parent/src/test/java/pl/sda/springparent/SpringParentApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sda.springparent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringParentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
