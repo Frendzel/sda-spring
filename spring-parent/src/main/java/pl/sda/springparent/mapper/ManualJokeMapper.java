67f81ae336e2715e931e2c3c4af08aa09e3b38e1
marcinerbel <marcinerbel90@gmail.com>
2019-07-27 13:47:33 +0200

    1 zajecia - roboczo

--- spring-parent/src/main/java/pl/sda/springparent/mapper/ManualJokeMapper.java
+++ spring-parent/src/main/java/pl/sda/springparent/mapper/ManualJokeMapper.java
@@ -2,6 +2,7 @@ package pl.sda.springparent.mapper;
 
 import pl.sda.springparent.dao.JokeEntity;
 import pl.sda.springparent.dto.Joke;
+import pl.sda.springparent.dto.JokeValue;
 
 import static java.util.Arrays.asList;
 
@@ -10,6 +11,16 @@ public class ManualJokeMapper {
         return JokeEntity.builder()
                 .value(joke.getValue().getJoke())
                 .categories(asList(joke.getValue().getCategories()))
+                .build();
+    }
+
+    public static Joke map(JokeEntity entity) {
+        return Joke.builder().type("SUCCESS")
+                .value(JokeValue.builder()
+                        .id(0)
+                        .joke(entity.getValue())
+                        .categories((String[]) entity.getCategories().toArray()) //nieładnie :(
+                        .build())
                 .build();
     }
 }
--- spring-parent/src/main/java/pl/sda/springparent/service/JokesService.java
+++ spring-parent/src/main/java/pl/sda/springparent/service/JokesService.java
@@ -2,6 +2,9 @@ package pl.sda.springparent.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.sda.springparent.dao.JokeEntity;
+import pl.sda.springparent.dto.Joke;
+import pl.sda.springparent.mapper.ManualJokeMapper;
 import pl.sda.springparent.repository.DbApi;
 
 @Service
@@ -9,5 +12,15 @@ public class JokesService {
 
     @Autowired
     DbApi mockedDb;
+
+    public Joke getJoke(Integer id) {
+        JokeEntity joke = mockedDb.getJoke(id);
+        return ManualJokeMapper.map(joke);
+    }
+
+    public void addJoke(Joke joke) {
+        JokeEntity entity = ManualJokeMapper.map(joke);
+        mockedDb.addJoke(entity);
+    }
 
 }
